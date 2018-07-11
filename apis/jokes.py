77b6405b29e0d009dc4dc8e9ca485fe3df7b9cfa
William Hill <hill119@llnl.gov>
2018-07-11 16:15:40 -0700

    added while loop to keep getting jokes

--- apis/jokes.py
+++ apis/jokes.py
@@ -2,8 +2,18 @@ import requests
 import time
 import json
 
-response = requests.get("https://08ad1pao69.execute-api.us-east-1.amazonaws.com/dev/random_joke")
-print "joke:", response.json()["setup"]
-# print "joke type:", type(response.content)
-time.sleep(1.5)
-print response.json()["punchline"]
+while True:
+    response = requests.get("https://08ad1pao69.execute-api.us-east-1.amazonaws.com/dev/random_joke")
+    print "joke:", response.json()["setup"]
+    # print "joke type:", type(response.content)
+    time.sleep(2.0)
+    print response.json()["punchline"]
+
+    another_one = raw_input("Would you like to hear another joke? ")
+    if another_one.lower() in ["yes", "y"]:
+        continue
+    elif another_one.lower() in ["no", "n"]:
+        break
+    else:
+        print "Invalid input so I'm going to tell you another joke anyway"
+        continue
