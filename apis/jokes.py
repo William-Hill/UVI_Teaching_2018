d1a0a5597c4263ef62ec5d40f3b5fed9e195195c
William Hill <mjh2225@gmail.com>
2018-07-16 21:16:21 -0400

    modified for Tuesday's lesson

--- apis/jokes.py
+++ apis/jokes.py
@@ -2,18 +2,23 @@ import requests
 import time
 import json
 
-while True:
-    response = requests.get("https://08ad1pao69.execute-api.us-east-1.amazonaws.com/dev/random_joke")
-    print "joke:", response.json()["setup"]
-    # print "joke type:", type(response.content)
-    time.sleep(2.0)
-    print response.json()["punchline"]
+def get_joke():
+    while True:
+        response = requests.get("https://08ad1pao69.execute-api.us-east-1.amazonaws.com/dev/random_joke")
+        print(response.json()["setup"])
+        time.sleep(2.0)
+        print(response.json()["punchline"])
 
-    another_one = raw_input("Would you like to hear another joke? ")
-    if another_one.lower() in ["yes", "y"]:
-        continue
-    elif another_one.lower() in ["no", "n"]:
-        break
-    else:
-        print "Invalid input so I'm going to tell you another joke anyway"
-        continue
+        another_one = input("Would you like to hear another joke? ")
+        if another_one.lower() in ["yes", "y"]:
+            continue
+        elif another_one.lower() in ["no", "n"]:
+            break
+        else:
+            print("Invalid input so I'm going to tell you another joke anyway")
+            continue
+def main():
+    get_joke()
+
+
+main()
