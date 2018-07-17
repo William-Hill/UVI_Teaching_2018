12b063534dacf032e5d558496f6c1bed3b7a34e3
William Hill <mjh2225@gmail.com>
2018-07-17 12:50:52 -0400

    added blank files for APIs lesson'

--- apis/gender_guess.py
+++ apis/gender_guess.py
@@ -1,6 +1,1 @@
 import requests
-
-name = input("Enter a name to guess its gender: ")
-response = requests.get("https://api.genderize.io/?name={}".format(name))
-
-print("response:", response.json())
