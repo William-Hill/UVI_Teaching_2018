be3f19ba50476a224a004f1cdd0c35a76d4bf68f
Rachael <rbrooks6@hawk.iit.edu>
2018-07-18 12:45:46 -0400

    All bugs corrected, final teachable chatbot

--- chatbot/drakechatbot.py
+++ chatbot/drakechatbot.py
@@ -1,46 +1,78 @@
 import random
 import string
 
-#TODO: add a goodbye
 drakelyrics = ['godsplan.txt', 'backtoback.txt', 'hotlinebling.txt', 'inmyfeelings.txt']
 
 i_words = []
 my_words = []
+
 remaining = []
+
 greetings = ['hello', 'hi', 'hey!' ,'hey', 'yo']
 goodbyes = ['bye', 'goodbye']
 line_bank = []
-bad_responses = ['', ' ', None]
-#TODO: use tensorflow to create a model to map words to each other
+
 
 def song_lyrics_parse():
     for song in drakelyrics:
-        f = open('drakelyrics/clean_' + song, 'r')
-        #TODO: make it more "random"
-        for line in f:
+        file = open('drakelyrics/clean_' + song, 'r')
+        for line in file:
             if '[' in line:
                 continue
+
             if line.strip('\n') in line_bank:
                 continue
             line_l = line.translate(str.maketrans('','',string.punctuation))
             line_l = line_l.split()
+
             if len(line_l) > 0:
+                response_found = False
                 for l in line_l:
                     line_bank.append(line.strip('\n'))
                     if l.lower() in ['i', 'im']:
                         i_words.append(line.strip('\n'))
+                        response_found = True
                         break
                     elif l.lower() in ['my', 'mine', 'me']:
                         my_words.append(line.strip('\n'))
+                        response_found = True
                         break
-                    else:
-                        remaining.append(line.strip('\n'))
-                        break
-        #print('i_words: {}\n, my_words: {}\n, remaining: {}\n'.format(i_words, my_words, remaining))
-    f.close()
+                if response_found == False:
+                    remaining.append(line.strip('\n'))
+        #print('i_words: {}\n, my_words: {}\n, remaining: {}\n'.format(i_words, my_words,remaining))
+    file.close()
 
-def process_response(userinput, response_found = False):
-    pass
+def process_response(userinput):
+    if userinput.strip() is '':
+        return("Are you still there?")
+    userinput_l = userinput.translate(str.maketrans('','',string.punctuation))
+    userinput_l = userinput_l.split(' ')
+
+
+    response_found = False
+    for u in userinput_l:
+        if u.lower() in greetings:
+            response_found = True
+            return(random.choice(greetings))
+
+        elif u.lower() in goodbyes:
+            response_found = True
+            print("I just hope that you miss me a little when I'm gone.")
+            quit()
+
+        elif u.lower() in ['you', 'you\'re', 'u', 'Drake']:
+            response_found = True
+            return(random.choice(i_words))
+
+        elif u.lower() in ['your', 'ur', 'yours']:
+            response_found = True
+            return(random.choice(my_words))
+
+        else:
+            continue
+
+    if response_found is False:
+        return(random.choice(remaining))
 
 def main():
 
@@ -49,36 +81,8 @@ def main():
     print("Hey! I'm Drake.")
 
     while True:
-        response_found = False
         userinput = input(">> ")
-        if userinput.strip() is '':
-            print("Are you still there?")
-            continue
-        userinput_l = userinput.translate(str.maketrans('','',string.punctuation))
-        userinput_l = userinput_l.split(' ')
-        for u in userinput_l:
-            if u.lower() in greetings:
-                response_found = True
-                print(random.choice(greetings))
-                break
-            elif u.lower() in goodbyes:
-                response_found = True
-                print("I just hope that you miss me a little when I'm gone.")
-                return
-            elif u.lower() in ['you', 'your', 'u', 'Drake']:
-                response_found = True
-                print(random.choice(i_words))
-                break
-
-            elif u.lower() in ['you\'re', 'youre', 'ur']:
-                response_found = True
-                print(random.choice(my_words))
-                break
-
-            else:
-                continue
-        if response_found is False:
-            print(random.choice(remaining))
+        print(process_response(userinput))
 
 
 if __name__ == '__main__':
