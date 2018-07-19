be2df8ccb1bf473d10c9e794950809e38b122889
William Hill <mjh2225@gmail.com>
2018-07-19 09:46:58 -0400

    fixed bugs from switch to Python 3

--- black_panther/wakanda_name.py
+++ black_panther/wakanda_name.py
@@ -2,8 +2,9 @@ from fuzzywuzzy import fuzz
 import re
 import string
 import collections
-import gender_guesser.detector as gender
+import gender_guesser.detector
 import random
+
 vowels = ['o', 'i', 'y', 'e', 'a','u']
 consonants =['b', 'c', 'd', 'f', 'g', 'h', 'j', 'k', 'l', 'm' ,'n' ,'p', 'q', 'r', 's', 't', 'v', 'w', 'x', 'z']
 wakandan_masculine_names = ["T'Challa", "T'Chaka", "W'Kabi", "N'Jobu", "N'Jadaka", "M'Baku", "Zuri", "Azzuri",
@@ -24,15 +25,16 @@ run name through vowel cipher
 run name through consonant cipher
 '''
 
-def anti_vowel(s):
-    result = re.sub(r'[AEIOU]', '', s, flags=re.IGNORECASE)
-    result = result.translate(None, string.punctuation)
+def anti_vowel(name):
+    '''Strips away vowels from a name'''
+    result = re.sub(r'[AEIOU]', '', name, flags=re.IGNORECASE)
+    table = str.maketrans({key: None for key in string.punctuation})
+    result = result.translate(table)
     return result
 
 def translate_name(your_name, similar_name):
     your_consonants = anti_vowel(your_name)
     similar_consonants = anti_vowel(similar_name)
-    print "similar_consonants:", similar_consonants
 
     if len(your_consonants) > len(similar_consonants):
         length_difference = len(your_consonants) - len(similar_consonants)
@@ -41,12 +43,10 @@ def translate_name(your_name, similar_name):
         length_difference = len(similar_consonants) - len(your_consonants)
         random.seed(length_difference)
         for _ in range(length_difference):
-            print "random_choice:", random.choice(consonants)
             your_consonants = your_consonants + random.choice(consonants)
 
-    print "your_consonants:", your_consonants
 
-    translation_table = string.maketrans(similar_consonants, your_consonants)
+    translation_table = str.maketrans(similar_consonants, your_consonants)
     return similar_name.translate(translation_table)
 
 def get_masculine_name(your_name):
@@ -59,8 +59,7 @@ def get_masculine_name(your_name):
             similar_name_score = score
             most_similar_name = name
 
-    print "similar_name_score", similar_name_score
-    print "most_similar_name", most_similar_name
+    print("most_similar_name", most_similar_name)
     return most_similar_name
 
 def get_feminine_name(your_name):
@@ -73,29 +72,31 @@ def get_feminine_name(your_name):
             similar_name_score = score
             most_similar_name = name
 
-    print "similar_name_score", similar_name_score
-    print "most_similar_name", most_similar_name
+    print("most_similar_name", most_similar_name)
 
     return most_similar_name
 
+def main():
+    your_name = input("Enter your name: ").strip()
+    d = gender_guesser.detector.Detector()
+    gender = d.get_gender(your_name)
+    print("gender:", gender)
+    if gender == "unknown" or gender == "andy":
+        gender = input("Is your name masculine or feminine? ")
 
-name_string = ''.join(wakandan_masculine_names)
-print "name_string:", name_string
-consonant_string = name_string.translate(None, "".join(vowels + ["'"]))
-print "consonant_string:", consonant_string
-print collections.Counter(consonant_string).most_common(3)
+    while True:
+        if gender.lower() in ["male", "m", "masculine", "mostly_male"]:
+            similar_name = get_masculine_name(your_name)
+            break
+        elif gender.lower() in ["f", "female", "feminine", "mostly_female"]:
+            similar_name = get_feminine_name(your_name)
+            break
+        else:
+            print("Please pick a valid gender")
+
+    wakandan_name = translate_name(your_name, similar_name)
+    print("wakandan_name: ", wakandan_name)
 
 
-your_name = raw_input("Enter your name: ").strip()
-d = gender.Detector()
-gender = d.get_gender(your_name)
-if gender == "unknown":
-    gender = raw_input("Is your name masculine or feminine? ")
-
-if gender == "male":
-    similar_name = get_masculine_name(your_name)
-else:
-    similar_name = get_feminine_name(your_name)
-
-translate_name = translate_name(your_name, similar_name)
-print "translated_name: ", translate_name
+if __name__ == '__main__':
+    main()
